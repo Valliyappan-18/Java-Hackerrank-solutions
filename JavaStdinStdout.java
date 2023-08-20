0ae055c35e75d1fdb8160ae2214bf67a904d4f7a
Valliyappan-p <p.valliyappan02@gmail.com>
2023-08-20 17:28:37 +0530

    JavaStdinStdout-Problem2

---
+++ JavaStdinStdout.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class JavaStdinStdout{   //change the classname as per given class name
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int a1 = scan.nextInt();
+        int a2 = scan.nextInt();
+        System.out.println(a);
+        System.out.println(a1);
+        System.out.println(a2);
+    }
+}
